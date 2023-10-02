e74ba875550975364d1652bb50d5275db99de8c3
bryanchopin <bryan.oliveros17@tectijuana.edu.mx>
2023-10-01 22:20:50 -0700

    Set the web sockets correctly into the app

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -1,47 +1,97 @@
-import { Server } from "socket.io";
+// import { Server as ServerIO } from "socket.io";
+
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
+
+// const sharedState = {
+//   value: 10,
+// };
+
+// export default async function handler(req, res) {
+//   if (!res.socket.server.io) {
+//     console.log("New Socket.io server...");
+
+//     // adapt Next's net Server to http Server
+//     const httpServer = res.socket.server;
+//     const io = new ServerIO(httpServer, {
+//       path: "/api/socket",
+//     });
+
+//     io.on("connection", (socket) => {
+//       console.log("Cliente WebSocket conectado");
+
+//       socket.emit("welcome", "¡Conexión exitosa con el servidor!");
+
+//       // Escucha cuando un cliente emite el evento 'changeValue'
+//       socket.on("changeValue", (newValue) => {
+//         // Actualiza el valor compartido
+//         sharedState.value = newValue;
+
+//         // Emite el nuevo valor a todos los clientes conectados
+//         io.emit("updateValue", sharedState.value);
+
+//         console.log(`Valor actualizado a: ${sharedState.value}`);
+//       });
+
+//       // Maneja la desconexión de un cliente WebSocket
+//       socket.on("disconnect", () => {
+//         console.log("Cliente WebSocket desconectado");
+//       });
+//     });
+
+//     res.socket.server.io = io;
+//   }
+//   res.end();
+// }
+
+
+import { Server as ServerIO } from "socket.io";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const sharedState = {
+  value: 10,
+};
 
 export default async function handler(req, res) {
-  if (!res.socket.server) {
-    return res.status(404).end();
+  if (!res.socket.server.io) {
+    console.log("New Socket.io server...");
+
+    // Adaptar el servidor net de Next a un servidor HTTP
+    const httpServer = res.socket.server;
+    const io = new ServerIO(httpServer, {
+      path: "/api/socket",
+    });
+
+    io.on("connection", (socket) => {
+      console.log("Cliente WebSocket conectado");
+
+      socket.emit("welcome", "¡Conexión exitosa con el servidor!");
+
+      // Escucha cuando un cliente emite el evento 'changeValue'
+      socket.on("changeValue", (newValue) => {
+        // Actualiza el valor compartido
+        sharedState.value = newValue;
+        console.log(`Valor actualizado a: ${newValue}`);
+
+        // Emite el nuevo valor a todos los clientes conectados
+        io.emit("updateValue", sharedState.value);
+      });
+
+      // Maneja la desconexión de un cliente WebSocket
+      socket.on("disconnect", () => {
+        console.log("Cliente WebSocket desconectado");
+      });
+    });
+
+    res.socket.server.io = io;
   }
-  const sharedState = {
-    value: 10,
-  };
-
-  // const io = new Server(res.socket.server);
-
-  const io = new Server(res.socket.server, {
-    cors: {
-      origin: "*",
-    },
-  });
-
-  res.socket.server.io = io;
-
-  io.on("connection", (socket) => {
-    console.log("Cliente WebSocket conectado");
-
-    socket.emit("welcome", "¡Conexión exitosa con el servidor!");
-
-    // Escucha cuando un cliente emite el evento 'changeValue'
-    socket.on("changeValue", (newValue) => {
-      // Actualiza el valor compartido
-      sharedState.value = newValue;
-
-      // Emite el nuevo valor a todos los clientes conectados
-      io.emit("updateValue", sharedState.value);
-
-      console.log(`Valor actualizado a: ${sharedState.value}`);
-    });
-
-    // Maneja la desconexión de un cliente WebSocket
-    socket.on("disconnect", () => {
-      console.log("Cliente WebSocket desconectado");
-    });
-  });
-
-  console.log("Servidor WebSocket en funcionamiento");
-
   res.end();
 }
-
