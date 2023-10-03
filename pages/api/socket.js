1ac163ed3493fa40985afaab327835bace230e3d
bryanchopin <bryan.oliveros17@tectijuana.edu.mx>
2023-10-02 21:45:01 -0700

    test new version 8.0

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -1,3 +1,52 @@
+// import { Server } from "socket.io";
+
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
+
+// const sharedState = {
+//   value: 10,
+// };
+
+// export default async function handler(req, res) {
+//   if (!res.socket.server.io) {
+//     console.log("New Socket.io server...");
+
+//     // Adaptar el servidor net de Next a un servidor HTTP
+//     const httpServer = res.socket.server;
+//     const io = new Server(httpServer, {
+//       path: "/api/socket",
+//     });
+
+//     io.on("connection", (socket) => {
+//       console.log("Cliente WebSocket conectado");
+
+//       socket.emit("welcome", "¡Conexión exitosa con el servidor!");
+
+//       // Escucha cuando un cliente emite el evento 'changeValue'
+//       socket.on("changeValue", (newValue) => {
+//         // Actualiza el valor compartido
+//         sharedState.value = newValue;
+//         console.log(`Valor actualizado a: ${newValue}`);
+
+//         // Emite el nuevo valor a todos los clientes conectados
+//         io.emit("updateValue", sharedState.value);
+//       });
+
+//       // Maneja la desconexión de un cliente WebSocket
+//       socket.on("disconnect", () => {
+//         console.log("Cliente WebSocket desconectado");
+//       });
+//     });
+
+//     res.socket.server.io = io;
+//   }
+//   res.end();
+// }
+
+
 import { Server } from "socket.io";
 
 export const config = {
@@ -6,21 +55,24 @@ export const config = {
   },
 };
 
+// Variable global para almacenar el servidor WebSocket
+let ioInstance = null;
+
 const sharedState = {
   value: 10,
 };
 
 export default async function handler(req, res) {
-  if (!res.socket.server.io) {
-    console.log("New Socket.io server...");
+  if (!ioInstance) {
+    console.log("Configurando el servidor WebSocket...");
 
     // Adaptar el servidor net de Next a un servidor HTTP
     const httpServer = res.socket.server;
-    const io = new Server(httpServer, {
+    ioInstance = new Server(httpServer, {
       path: "/api/socket",
     });
 
-    io.on("connection", (socket) => {
+    ioInstance.on("connection", (socket) => {
       console.log("Cliente WebSocket conectado");
 
       socket.emit("welcome", "¡Conexión exitosa con el servidor!");
@@ -32,7 +84,7 @@ export default async function handler(req, res) {
         console.log(`Valor actualizado a: ${newValue}`);
 
         // Emite el nuevo valor a todos los clientes conectados
-        io.emit("updateValue", sharedState.value);
+        ioInstance.emit("updateValue", sharedState.value);
       });
 
       // Maneja la desconexión de un cliente WebSocket
@@ -40,8 +92,8 @@ export default async function handler(req, res) {
         console.log("Cliente WebSocket desconectado");
       });
     });
-
-    res.socket.server.io = io;
   }
+
+  res.socket.server.io = ioInstance;
   res.end();
 }
