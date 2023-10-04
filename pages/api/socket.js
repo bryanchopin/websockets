ec12735257deeb32c9c6ecd45f115a93b0826503
bryanchopin <bryan.oliveros17@tectijuana.edu.mx>
2023-10-04 10:32:39 -0700

    test new update 13.0

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -97,6 +97,106 @@
 //   res.socket.server.io = ioInstance;
 //   res.end();
 // }
+
+
+
+
+
+// import { Server } from "socket.io";
+
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
+
+// // Variable global para almacenar el servidor WebSocket
+// let ioInstance = null;
+
+// const sharedState = {
+//   value: 10,
+// };
+
+// export default async function handler(req, res) {
+//   if (!ioInstance) {
+//     console.log("Configurando el servidor WebSocket...");
+
+//     // const ACCEPTED_ORIGINS = [
+//     //   'http://localhost:3000',
+//     //   'https://websockets-xi.vercel.app/'
+//     // ];
+//     // const origin = req.headers.origin;
+
+//     // if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+//     //   // Verifica si origin es undefined y, si lo es, establece el valor a '*' para permitir cualquier origen.
+//     //   const allowedOrigin = origin || '*';
+//     //   res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
+//     // }
+
+//     // // Adaptar el servidor net de Next a un servidor HTTP
+//     // const httpServer = res.socket.server;
+//     // ioInstance = new Server(httpServer, {
+//     //   path: "/api/socket",
+//     //   cors: {
+//     //     origin: "*",
+//     //   },
+//     // });
+
+//     const ACCEPTED_ORIGINS = [
+//       'http://localhost:3000',
+//       'https://websockets-xi.vercel.app/api/socket'
+//     ];
+//     const origin = req.headers.origin;
+    
+//     if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+//       // Verifica si origin es undefined y, si lo es, establece el valor a '*' para permitir cualquier origen.
+//       const allowedOrigin = origin || '*';
+//       res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
+//     }
+    
+//     // Adaptar el servidor net de Next a un servidor HTTP
+//     const httpServer = res.socket.server;
+//     ioInstance = new Server(httpServer, {
+//       path: "/api/socket",
+//       cors: {
+//         origin: ACCEPTED_ORIGINS, // Utiliza la lista de orígenes permitidos
+//         methods: ["GET", "POST"], // Define los métodos permitidos
+//       },
+//     });
+
+//     ioInstance.on("connection", (socket) => {
+//       console.log("Cliente WebSocket conectado");
+
+//       socket.emit("welcome", "¡Conexión exitosa con el servidor!");
+
+//       if (!ioInstance) {
+//         // Evitar configurar más de una vez en caso de múltiples conexiones simultáneas
+//         return;
+//       }
+
+//       // Escucha cuando un cliente emite el evento 'changeValue'
+//       socket.on("changeValue", (newValue) => {
+//         // Actualiza el valor compartido
+//         sharedState.value = newValue;
+//         console.log(`Valor actualizado a: ${newValue}`);
+
+//         // Emite el nuevo valor a todos los clientes conectados
+//         ioInstance.emit("updateValue", sharedState.value);
+//       });
+
+//       // Maneja la desconexión de un cliente WebSocket
+//       socket.on("disconnect", () => {
+//         console.log("Cliente WebSocket desconectado");
+//       });
+//     });
+//   }
+
+//   res.socket.server.io = ioInstance;
+//   res.end();
+// }
+
+
+
 import { Server } from "socket.io";
 
 export const config = {
@@ -108,75 +208,31 @@ export const config = {
 // Variable global para almacenar el servidor WebSocket
 let ioInstance = null;
 
-const sharedState = {
-  value: 10,
-};
-
 export default async function handler(req, res) {
   if (!ioInstance) {
     console.log("Configurando el servidor WebSocket...");
 
-    // const ACCEPTED_ORIGINS = [
-    //   'http://localhost:3000',
-    //   'https://websockets-xi.vercel.app/'
-    // ];
-    // const origin = req.headers.origin;
-
-    // if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
-    //   // Verifica si origin es undefined y, si lo es, establece el valor a '*' para permitir cualquier origen.
-    //   const allowedOrigin = origin || '*';
-    //   res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
-    // }
-
-    // // Adaptar el servidor net de Next a un servidor HTTP
-    // const httpServer = res.socket.server;
-    // ioInstance = new Server(httpServer, {
-    //   path: "/api/socket",
-    //   cors: {
-    //     origin: "*",
-    //   },
-    // });
-
-    const ACCEPTED_ORIGINS = [
-      'http://localhost:3000',
-      'https://websockets-xi.vercel.app/api/socket'
-    ];
-    const origin = req.headers.origin;
-    
-    if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
-      // Verifica si origin es undefined y, si lo es, establece el valor a '*' para permitir cualquier origen.
-      const allowedOrigin = origin || '*';
-      res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
-    }
-    
     // Adaptar el servidor net de Next a un servidor HTTP
     const httpServer = res.socket.server;
+
+    // Configurar Socket.IO en el servidor HTTP
     ioInstance = new Server(httpServer, {
       path: "/api/socket",
       cors: {
-        origin: ACCEPTED_ORIGINS, // Utiliza la lista de orígenes permitidos
-        methods: ["GET", "POST"], // Define los métodos permitidos
+        origin: ["https://websockets-xi.vercel.app"], // Reemplaza con tus orígenes permitidos
+        methods: ["GET", "POST"],
       },
     });
 
     ioInstance.on("connection", (socket) => {
-      console.log("Cliente WebSocket conectado");
+      console.log("Cliente WebSocket conectado", socket);
 
-      socket.emit("welcome", "¡Conexión exitosa con el servidor!");
-
-      if (!ioInstance) {
-        // Evitar configurar más de una vez en caso de múltiples conexiones simultáneas
-        return;
-      }
+      socket.emit("welcome", "¡Conexión exitosa con el servidor! " + socket.id);
 
       // Escucha cuando un cliente emite el evento 'changeValue'
       socket.on("changeValue", (newValue) => {
-        // Actualiza el valor compartido
-        sharedState.value = newValue;
-        console.log(`Valor actualizado a: ${newValue}`);
-
         // Emite el nuevo valor a todos los clientes conectados
-        ioInstance.emit("updateValue", sharedState.value);
+        ioInstance.emit("updateValue", newValue);
       });
 
       // Maneja la desconexión de un cliente WebSocket
