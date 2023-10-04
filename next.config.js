b1a6d83492751f06b60f048bd79a3484d5c819f4
bryanchopin <bryan.oliveros17@tectijuana.edu.mx>
2023-10-04 13:06:52 -0700

    ya me canse mi bro

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  rewrites: async () => {
-    return [
-      {
-        source: "/api/socket",
-        destination: "http://localhost:3000/api/socket",
-      },
-    ];
-  },
 }
-
 module.exports = nextConfig
