020f7a709671d15e0eece35c0d52ef0497b4afa5
bryanchopin <bryan.oliveros17@tectijuana.edu.mx>
2023-10-04 22:27:16 -0700

    add rewrites in next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  //add something like a proxy to the socket server
+  async rewrites() {
+    return [
+      {
+        source: "/api/socket",
+        destination: process.env.SOCKET_SERVER_URL,
+      },
+    ];
+  },
 }
 module.exports = nextConfig
