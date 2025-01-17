e64a0c869d6c052d020219956e520f55d40ee81b
bryanchopin <bryan.oliveros17@tectijuana.edu.mx>
2025-01-17 15:24:28 -0800

    update README with basic socket interaction instructions and comment out rewrites in next.config

--- next.config.js
+++ next.config.js
@@ -2,13 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   //add something like a proxy to the socket server
-  async rewrites() {
-    return [
-      {
-        source: "/api/socket",
-        destination: process.env.SOCKET_SERVER_URL,
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/socket",
+  //       destination: process.env.SOCKET_SERVER_URL,
+  //     },
+  //   ];
+  // },
 }
 module.exports = nextConfig
